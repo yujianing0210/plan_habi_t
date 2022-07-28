7c6cce73e662a31bbfdef5be0fd2b381dbec1249
Beaumont Yun <b.yun@hotmail.com>
2022-07-28 16:09:52 +0800

    Adding CSS elements and HeroIcons

--- pages/signin.js
+++ pages/signin.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import logo from '../public/logo.jpg'
+import { LockClosedIcon } from '@heroicons/react/outline'
+import { UserIcon } from '@heroicons/react/outline'
 
 const Signin = () => {
     return (
@@ -9,17 +11,13 @@ const Signin = () => {
             </div>
             <div className="justify-self-center pt-90 flex flex-col gap-3 w-80">
                 <div className="w-full text-center text-xl font-bold my-10">Login</div>
-                <div className="form-control">
-                    <label className="input-group input-group-vertical">
-                        <span>Email</span>
-                        <input type="text" placeholder="info@site.com" className="input input-bordered bg-white" />
-                    </label>
+                <div className="form-control flex flex-row w-full">
+                    <UserIcon className='w-6 h-6 my-3 mr-5' />
+                    <input type="text" placeholder="info@site.com" className="input input-bordered bg-white w-full" />
                 </div>
-                <div className="form-control">
-                    <label className="input-group input-group-vertical">
-                        <span>Password</span>
-                        <input type="text" className="input input-bordered bg-white" />
-                    </label>
+                <div className="form-control flex flex-row w-full">
+                    <LockClosedIcon className='w-6 h-6 my-3 mr-5' />
+                    <input type="text" className="input input-bordered bg-white w-full" />
                 </div>
             </div>
             <div className="justify-self-center place-self-end pb-10">
